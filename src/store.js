849e200cf1e002d28f588f50f31d2bc449c12cc5
VBTX <jv.svedaite@gmail.com>
2019-07-19 14:30:32 -0500

    add currentUser reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux' 
 import usersReducer from './reducers/users'
+import currentUser from './reducers/currentUser'
 import  thunk  from 'redux-thunk'
 
 const reducer = combineReducers({
-	users: usersReducer})
+	users: usersReducer,
+	//above we have expanded version, below - shorthand
+	currentUser})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
