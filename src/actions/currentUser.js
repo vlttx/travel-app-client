45c1b50fe89e89f4e92200842c1689e9a2121ed7
VBTX <jv.svedaite@gmail.com>
2019-07-19 17:57:36 -0500

    add action to currentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -12,6 +12,12 @@
 //which as an argument receives
  export const login = credentials => {
  	return dispatch =>{
-		return fetch("http://localhost:3001")
+		return fetch("http://localhost:3001/api/v1/login",{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(credentials)
+		})
  	}
  }
