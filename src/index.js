b8f25bcd1b83153ae63cf453fc5a59ef55039e82
VBTX <jv.svedaite@gmail.com>
2019-07-19 12:43:27 -0500

    add fake store for testing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
+import  thunk  from 'redux-thunk'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const users= () => []
+const reducer = combineReducers({users})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
+ReactDOM.render(
+	<Provider store={store}>
+	<App />
+	</Provider>, 
+	document.getElementById('root'));
+
+// const reducers = combineReducers({
+// 	items: itemsReducer,
+// 	itemFormData
+// })
+// const middleware = [thunk]; best when we have a lot more middleware
+
+
+// export default createStore(reducers, composeEnhancer(applyMiddleware(...middleware)))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
