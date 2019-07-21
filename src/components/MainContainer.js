6736afdde29e72469fa23cae885354cda3bb0d9f
VBTX <jv.svedaite@gmail.com>
2019-07-21 13:50:03 -0500

    add MainContainer

---
+++ src/components/MainContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+const MainContainer = () => {
+	return (
+		<div className="MainContainer">
+			Main Container
+		</div>
+
+		)
+}
+
+export default MainContainer
