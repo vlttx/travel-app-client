1d3bbb69c755d27d7bd85e027f0ad5be2ec52852
VBTX <jv.svedaite@gmail.com>
2019-07-20 19:50:36 -0500

    write a get method for fetching current user

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -19,5 +19,41 @@
 			},
 			body: JSON.stringify(credentials)
 		})
+		.then(r => r.json())
+		.then(user => {
+			if (user.error) {
+				alert(user.error)
+			} else {
+				// dispatch({type: "SET_CURRENT_USER", user: user}) but we have action creator above, so
+				dispatch(setCurrentUser(user))
+			}
+
+
+			})
+		.catch(console.log)
  	}
+ }
+
+ 
+    export const getCurrentUser = credentials => {
+  return dispatch =>{
+    return fetch("http://localhost:3001/api/v1/login",{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+        // dispatch({type: "SET_CURRENT_USER", user: user}) but we have action creator above, so
+        dispatch(setCurrentUser(user))
+      }
+
+
+      })
+    .catch(console.log)
+  }
  }
