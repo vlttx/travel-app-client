801ca2e61bad7bd09dabdb873b3cb275b3e6e13b
VBTX <jv.svedaite@gmail.com>
2019-07-23 11:28:33 -0500

    add new trip reducer

---
+++ src/reducers/newTrip.js
@@ -0,0 +1,18 @@
+const initialState = {
+	name: "",
+	startDate: "",
+	endDate: ""
+}
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case "UPDATE_SIGNUP_FORM":
+		return action.formData
+
+		case "RESET_SIGNUP_FORM":
+		return initialState
+		
+		default:
+		  return state
+	}
+}
