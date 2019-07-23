71c51d79a4acb2854c4c7d206470e893bb3f41f0
VBTX <jv.svedaite@gmail.com>
2019-07-23 14:33:54 -0500

    import current user from state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Login from './LoginForm'
 import Signup from './SignUp'
+import { connect } from 'react-redux'
 import { Link } from "react-router-dom";
 import '../App.css';
 
 
-const Home = () => (
+const Home = ({currentUser}) => (
 	<div>
 	<h3>Welcome! Please <Link to="/signup" className="link">Sign Up</Link> or <Link to="/login" className="link">Log In</Link>!</h3>
 	</div>
 );
 
-export default Home;
+
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser,
+    loggedIn: !!state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Home)
