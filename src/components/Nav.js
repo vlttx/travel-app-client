880321a01b12f2c4accf6a68861e1abd40790802
VBTX <jv.svedaite@gmail.com>
2019-07-23 14:34:52 -0500

    add NavLinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Logout from './Logout'
 import Login from './LoginForm'
+import { NavLink } from 'react-router-dom'
 
 
-const Nav = ({currentUser}) => {
+const Nav = ({currentUser, loggedIn}) => {
 	return (
 		<div className="nav">
-		{currentUser ? <h2>Special welcome to {currentUser.name} from {currentUser.hometown.city}</h2>: ""}
-		{currentUser ? <Logout/> : <Login />}
+		<NavLink exact activeClass to="/my-trips">My Trips  |</NavLink>
+		<NavLink exact activeClass to="/my-trips/new">New Trip  |</NavLink>
+		{ loggedIn ? <Logout/> : null} 	
+		 <h2>Special welcome to {currentUser.name} from {currentUser.hometown.city}</h2>
 		</div>
 		)
 }
 const mapStateToProps = state => {
   return {
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    loggedIn: !!state.currentUser
   }
 }
 
 export default connect(mapStateToProps)(Nav)
+	// <NavLink exact activeClass to="/logout">Log Out</NavLink>
+
