a9237f27782c1d849d1d3fe2f30af1d22b6366e3
VBTX <jv.svedaite@gmail.com>
2019-07-24 10:44:53 -0500

    add links instead of all trip cards

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const TripCard = ({trip}) => {
@@ -9,7 +10,7 @@ const TripCard = ({trip}) => {
 
 		<div className="tripCard">
 		<h3>{trip.name} ({trip.start_date}-{trip.end_date})</h3> <br/>
-		<img src={trip.imageurl} alt={trip.name} width="500px" height="500px"/></div> :
+		<img src={trip.imageurl} alt={trip.name} width="500px" height="500px"/><br/><Link to={`/trips/${trip.id}/edit`}>Edit Trip Info</Link></div> :
 		null
 		)
 }
