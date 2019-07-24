216e0e82c2f2e8bf2ee6285871337a418d30e246
VBTX <jv.svedaite@gmail.com>
2019-07-24 10:12:36 -0500

    add ternary

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -4,9 +4,13 @@ import React from 'react'
 const TripCard = ({trip}) => {
 	
 	return (
+
+		trip ?
+
 		<div className="tripCard">
 		<h3>{trip.name} ({trip.start_date}-{trip.end_date})</h3> <br/>
-		<img src={trip.imageurl} alt={trip.name} width="500px" height="500px"/></div>
+		<img src={trip.imageurl} alt={trip.name} width="500px" height="500px"/></div> :
+		null
 		)
 }
 
