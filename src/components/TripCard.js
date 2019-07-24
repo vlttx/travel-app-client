4dfe17edd49b04eb7e2ddfa8766b79e0b83723fa
VBTX <jv.svedaite@gmail.com>
2019-07-23 20:54:22 -0500

    add a class

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const TripCard = ({trip}) => {
 	
 	return (
-		<div>
+		<div className="tripCard">
 		<h3>{trip.name} ({trip.start_date}-{trip.end_date})</h3> <br/>
 		<img src={trip.imageurl} alt={trip.name} width="500px" height="500px"/></div>
 		)
