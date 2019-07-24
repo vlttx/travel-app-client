c521f9b78623c6811fa4ee46e5eba09c675cf5f4
VBTX <jv.svedaite@gmail.com>
2019-07-24 11:25:25 -0500

    add action to setFormFataForEdit

--- src/actions/newTrip.js
+++ src/actions/newTrip.js
@@ -13,3 +13,16 @@ export const resetNewForm = () => {
 		type: "RESET_NEWTRIP_FORM"
 	}
 }
+
+export const setFormDataForEdit = trip => {
+	const tripData = {
+		name: trip.name,
+		startDate: trip.start_date,
+		endDate: trip.end_date,
+		imageurl: trip.imageurl
+	}
+	return {
+	type: "SET_FORM_DATA_FOR_EDIT",
+	tripData
+}
+}
